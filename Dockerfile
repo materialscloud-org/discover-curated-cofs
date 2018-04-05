26fab1f5a525657bce71d52d46cfe557ac982503
Leopold Talirz <leopold.talirz@gmail.com>
2018-04-05 18:47:18 +0200

    take advantage of postgres scipt

--- Dockerfile
+++ Dockerfile
@@ -2,24 +2,19 @@
 #
 FROM mc-docker-stack:discover
 
-# Copy COF database
-USER root
-WORKDIR /opt/cofdb
+
+# Download COF DB
+USER scientist
+WORKDIR /project
 RUN base_url=http://archive.materialscloud.org/file/2018.0003/v1;  \
     wget ${base_url}/cof-database.aiida;
-#COPY cof-database.aiida .
-COPY import-cofdb.sh .
-RUN  chown -R root:root /opt/cofdb/;                                \
-     chmod -R +r /opt/cofdb/;                                       \
-     chmod -R +x /opt/cofdb/import-cofdb.sh                         
-     
+
+# Import COF DB
+COPY import-cofdb.sh /opt/
+RUN /opt/import-cofdb.sh
+
+# start bokeh server
 EXPOSE 8888
-
-USER scientist
-
-RUN /opt/cofdb/import-cofdb.sh
-
-WORKDIR /project
 COPY start-singleuser.sh /opt/
 CMD ["/opt/start-singleuser.sh"]
 
--- import-cofdb.sh
+++ import-cofdb.sh
@@ -1,43 +1,17 @@
 #!/bin/bash -e
-
-# This script is executed whenever the docker container is (re)started.
-
-#===============================================================================
-# debuging
 set -x
 
 #===============================================================================
-#TODO setup signal handler which shuts down posgresql and aiida.
-PGBIN=/usr/lib/postgresql/9.6/bin
-
-# helper function to start psql and wait for it
-function start_psql {
-   ${PGBIN}/pg_ctl -D /project/.postgresql -l /project/.postgresql/logfile start
-   TIMEOUT=20
-   until psql -h localhost template1 -c ";" || [ $TIMEOUT -eq 0 ]; do
-      echo ">>>>>>>>> Waiting for postgres server, $((TIMEOUT--)) remaining attempts..."
-      tail -n 50 /project/.postgresql/logfile
-      sleep 1
-   done
-}
-
-# Start postgresql
-echo "" > /project/.postgresql/logfile # empty log files
-rm -vf /project/.postgresql/.s.PGSQL.5432
-rm -vf /project/.postgresql/.s.PGSQL.5432.lock
-rm -vf /project/.postgresql/postmaster.pid
-#${PGBIN}/pg_ctl -D /project/.postgresql stop || true
-start_psql
-
-# import COF datbaase
-for i in /opt/cofdb/*.aiida; do
-   verdi import $i
-done
+# start postgres
+. /opt/postgres.sh
 
 #===============================================================================
-# environment
-export PYTHONPATH=/project
-export SHELL=/bin/bash
+# Download COF database
+base_url=http://archive.materialscloud.org/file/2018.0003/v1;  \
+wget ${base_url}/cof-database.aiida
+verdi import cof-database.aiida > import.log
 
+# no need to keep the duplicate
+rm cof-database.aiida
 
 #EOF
