4ca03c59611794a4a10d289182a4cf6199268a9a
Leopold Talirz <leopold.talirz@gmail.com>
2018-04-05 19:04:10 +0200

    skip duplicate download of cof db

--- import-cofdb.sh
+++ import-cofdb.sh
@@ -6,9 +6,7 @@ set -x
 . /opt/postgres.sh
 
 #===============================================================================
-# Download COF database
-base_url=http://archive.materialscloud.org/file/2018.0003/v1;  \
-wget ${base_url}/cof-database.aiida
+# import COF database
 verdi import cof-database.aiida > import.log
 
 # no need to keep the duplicate
