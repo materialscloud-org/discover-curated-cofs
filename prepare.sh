c0c9e4511758aa098fc28177e6d8ca8cf63ca455
Leopold Talirz <leopold.talirz@gmail.com>
2019-02-05 20:19:42 +0100

    add test data

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Download jsmol
+echo "### Donwloading jsmol"
 wget https://sourceforge.net/projects/jmol/files/Jmol/Version%2014.29/Jmol%2014.29.22/Jmol-14.29.22-binary.zip/download --output-document jmol.zip
 unzip jmol.zip 
 cd jmol-14.29.22
@@ -10,18 +11,18 @@ cd ..
 rm -r jmol-14.29.22
 
 # Download data
+echo "### Downloading test data"
 cd data/
-export base_url=http://archive.materialscloud.org/file/2018.0003/v2; \
+export base_url=https://object.cscs.ch/v1/AUTH_b1d80408b3d340db9f03d373bbde5c1e/discover-cofs/test_data; \
     wget ${base_url}/structures.tgz &&\
-    wget ${base_url}/properties.tgz
-
+    wget ${base_url}/properties.csv
 
 # Extract data
-tar xf structures.tgz && rm structures.tgz && \
-    tar xf properties.tgz && rm properties.tgz
+tar xf structures.tgz && rm structures.tgz
 
 cd ..
 
 # Create sqlite DB
+echo "### Creating sqlite database"
 python import_db.py
 
