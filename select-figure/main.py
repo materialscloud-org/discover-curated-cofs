6cc6f9e599876daac247eea0a2dcd9675cd6da64
Leopold Talirz <leopold.talirz@gmail.com>
2019-02-14 11:27:01 +0100

    use bokeh template
     * include materials design css as part of the code
     * switch to proper use of jinja template

--- select-figure/main.py
+++ select-figure/main.py
@@ -7,10 +7,10 @@ from bokeh.layouts import layout
 import bokeh.models as bmd
 from bokeh.io import curdoc
 
-html = bmd.Div(
-    text=open(join(dirname(__file__), "static", "table.html")).read(),
-    width=960)
+#html = bmd.Div(
+#    text=open(join(dirname(__file__), "static", "table.html")).read(),
+#    width=960)
+#curdoc().add_root(layout([html]))
 
 # Put the tabs in the current document for display
 curdoc().title = "Covalent Organic Frameworks"
-curdoc().add_root(layout([html]))
