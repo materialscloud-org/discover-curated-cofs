d823b1b9946f5d7aa3bcdfcc151cd633cdb9e6eb
Leopold Talirz <leopold.talirz@gmail.com>
2019-06-24 22:44:20 +0200

    remove outdated code

--- figure/query.py
+++ figure/query.py
@@ -9,49 +9,6 @@ from six.moves import zip
 data_empty = dict(x=[0], y=[0], uuid=['1234'], color=[0], name=['no data'])
 
 
-def get_data_sqla(projections, sliders_dict, quantities):
-    """Query database using SQLAlchemy.
-    
-    :return: list of projected properties
-    :rtype: list
-
-    Note: For efficiency, this uses the the sqlalchemy.sql interface which does
-    not go via the (more convenient) ORM.
-    """
-    from import_db import automap_table, engine
-    from sqlalchemy.sql import select, and_
-
-    msg = ""
-
-    Table = automap_table(engine)
-
-    selections = []
-    for label in projections:
-        selections.append(getattr(Table, label))
-
-    filters = []
-    for k, v in sliders_dict.items():
-        if isinstance(v, RangeSlider):
-            if not v.value == quantities[k]['range']:
-                f = getattr(Table, k).between(v.value[0], v.value[1])
-                filters.append(f)
-        elif isinstance(v, CheckboxButtonGroup):
-            if not len(v.active) == len(v.labels):
-                f = getattr(Table, k).in_([v.tags[i] for i in v.active])
-                filters.append(f)
-
-    s = select(selections).where(and_(*filters))
-
-    results = engine.connect().execute(s).fetchall()
-
-    return results
-
-link_attribute_dict = {
-    'opt_out_pe': ['PE', 'Td', 'Pd', 'WCg', 'WCv', 'Pur'],
-    'opt_out_zeopp': ['Density', 'ASA_m^2/g', 'AV_Volume_fraction'],
-    'iso_co2': ['henry_coefficient_average'],
-}
-
 def get_data_aiida(projections, sliders_dict, quantities):
     """Query the AiiDA database"""
     from aiida import load_dbenv, is_dbenv_loaded
