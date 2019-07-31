4e5da10d9cbe2f46ba4d1a3d0b392093d1fa1600
Daniele Ongari <daniele.ongari@gmail.com>
2019-07-31 14:52:04 +0200

    Improved figure plot, removed deprecated comments (#12)

--- figure/query.py
+++ figure/query.py
@@ -9,9 +9,10 @@ from six.moves import zip
 data_empty = dict(x=[0], y=[0], uuid=['1234'], color=[0], name=['no data'])
 
 link_attribute_dict = {
-    'opt_out_pe': ['PE', 'Td', 'Pd', 'WCg', 'WCv', 'Pur'],
+    'opt_out_pe': ['PE', 'WCg', 'WCv', 'Pur'],
     'opt_out_zeopp': ['Density', 'ASA_m^2/g', 'AV_Volume_fraction'],
-    'iso_co2': ['henry_coefficient_average'],
+    'iso_co2': ['henry_coefficient_average'], #TODO: handle also the uptake at 30 bar
+    #'iso_n2': ['henry_coefficient_average'], TODO: redesign the query so that this can be added
 }
 
 def get_data_aiida(projections, sliders_dict, quantities):
@@ -26,71 +27,16 @@ def get_data_aiida(projections, sliders_dict, quantities):
     from aiida.orm import WorkCalculation, Node
 
     filters = {}
-    #projections = projections[:3]
-
-    #def add_range_filter(bounds, label):
-    #    # a bit of cheating until this is resolved
-    #    # https://github.com/aiidateam/aiida_core/issues/1389
-    #    #filters['attributes.'+label] = {'>=':bounds[0]}
-    #    filters['attributes.' + label] = {
-    #        'and': [{
-    #            '>=': bounds[0]
-    #        }, {
-    #            '<': bounds[1]
-    #        }]
-    #    }
-
-    #for k, v in sliders_dict.items():
-    #    # Note: filtering is costly, avoid if possible
-    #    if not v.value == quantities[k]['range']:
-    #        add_range_filter(v.value, k)
     
     qb = QueryBuilder()
     qb.append(WorkCalculation, filters={ 'attributes.function_name': {'==': 'collect_outputs'} }, tag='collect')
-
-    # Note: This implementation is faster but does not preserve the order of projections 
-    #       Could still use it but would need to reorder the columns before returning.
-    # qb.append(WorkCalculation, filters={ 'attributes.function_name': {'==': 'collect_outputs'} }, tag='collect')
-    # for link_label in link_attribute_dict:
-    #     link_projections = [ 'attributes.' + p for p in projections if p in link_attribute_dict[link_label]]
-    #     if link_projections:
-    #         print(link_projections)
-    #         qb.append(ParameterData, project=link_projections, edge_filters={'label': link_label}, input_of='collect')
 
     for p in projections:
         for link_label in link_attribute_dict:
             if p in link_attribute_dict[link_label]:
                print(p)
                qb.append(ParameterData, project=['attributes.' + p], edge_filters={'label': link_label}, input_of='collect')
-
-    #     if link_projections:
-    #         print(link_projections)
-    #         qb.append(ParameterData, project=link_projections, edge_filters={'label': link_label}, input_of='collect')
     qb.append(StructureData, project=['label', 'uuid'], edge_filters={'label': 'ref_structure'}, input_of='collect')    
 
     return qb.all()
 
-    #nresults = qb.count()
-    #if nresults == 0:
-    #    plot_info.text = "No matching COFs found."
-    #    return data_empty
-
-    #plot_info.text = "{} COFs found. Plotting...".format(nresults)
-
-
-    ## x,y position
-    #x, y, clrs, uuids, names, cif_uuids = list(zip(*qb.all()))
-    #plot_info.text = "{} COFs queried".format(nresults)
-    #x = list(map(float, x))
-    #y = list(map(float, y))
-    #cif_uuids = list(map(str, cif_uuids))
-    #uuids = list(map(str, uuids))
-
-    #if projections[2] == 'bond_type':
-    #    #clrs = map(lambda clr: bondtypes.index(clr), clrs)
-    #    clrs = list(map(str, clrs))
-    #else:
-    #    clrs = list(map(float, clrs))
-
-    #return qb.all()
-    #return dict(x=x, y=y, uuid=cif_uuids, color=clrs, name=names)
