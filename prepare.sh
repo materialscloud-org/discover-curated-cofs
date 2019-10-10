b9257502e4245320feda6088b0fd0b7bdeccb5fa
Leopold Talirz <leopold.talirz@gmail.com>
2019-10-10 20:51:40 +0200

    switch to local jsmol
    was using external copy of jsmol library

--- prepare.sh
+++ prepare.sh
@@ -9,20 +9,3 @@ unzip jsmol.zip
 mv jsmol ../detail/static
 cd ..
 rm -r jmol-14.29.22
-
-# Download data
-echo "### Downloading test data"
-cd data/
-export base_url=https://object.cscs.ch/v1/AUTH_b1d80408b3d340db9f03d373bbde5c1e/discover-cofs/test_data; \
-    wget ${base_url}/structures.tgz &&\
-    wget ${base_url}/properties.csv
-
-# Extract data
-tar xf structures.tgz && rm structures.tgz
-
-cd ..
-
-# Create sqlite DB
-echo "### Creating sqlite database"
-python import_db.py
-
