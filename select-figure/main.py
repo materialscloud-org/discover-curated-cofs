93b3200cc4e09ec491554f988e1561ec721f7cdd
Daniele Ongari <daniele.ongari@gmail.com>
2020-04-27 10:32:58 +0200

    select-figure does not need csv files

--- select-figure/main.py
+++ select-figure/main.py
@@ -1,11 +1,12 @@
-# coding: utf-8
 """Provenance table"""
 
 import os
-import pandas
+import pandas as pd
 import panel as pn
-from panel.interact import interact
+#from panel.interact import interact
 
+from aiida.orm.querybuilder import QueryBuilder
+from aiida.orm import Node, Group
 from aiida import load_profile
 load_profile()
 
@@ -14,97 +15,89 @@ try:
 except:
     this_dir = ''
 
+TAG_KEY = "tag4"
+
 
 def provenance_link(uuid, label=None):
     """Return representation of provenance link."""
-    import os
 
     if label is None:
         label = "Browse provenance\n" + str(uuid)
 
     logo_url = "select-figure/static/images/aiida-128.png"
     explore_url = os.getenv('EXPLORE_URL', "https://dev-www.materialscloud.org/explore/curated-cofs")
-    return "<a href='{url}/details/{uuid}' target='_blank'><img src={logo_url} title='{label}' style='width: 20px;  height: auto;'></a>".format(
+    return "<a href='{url}/details/{uuid}' target='_blank'><img src={logo_url} title='{label}' style='width: 20px;  height: auto;'></a>".format(  # noqa
         url=explore_url, uuid=str(uuid), label=label, logo_url=logo_url)
 
 
-def detail_link(label):
+def detail_link(mat_id):
     """Return representation of provenance link."""
     logo_url = 'select-figure/static/images/co2-128.png'
-    return "<a href='detail?id={l}' target='_blank'><img src='{u}' style='width: 20px;  height: auto;'></a>".format(
-        l=label, u=logo_url)
+    return "<a href='detail?id={}' target='_blank'><img src='{}' style='width: 20px;  height: auto;'></a>".format(
+        mat_id, logo_url)
 
 
-def get_paper_link(label, name, df):
-    """Return link of the article."""
-    paper_id = "p{:s}".format(label[:4])
-    paper_row = df.loc[df["CURATED-COFs paper ID"] == paper_id]
-    link_paper = "https://doi.org/" + paper_row["DOI"].values[0]
-    return "<a href='{}' target='_blank'>{}</a>".format(link_paper, name)
+def doi_link(mat_dict):
+    """Return the DOI link of the article."""
+    name = mat_dict['orig_cif'].extras['name_conventional']
+    doi = mat_dict['orig_cif'].extras['doi_ref']
+    return "<a href='https://doi.org/{}' target='_blank'>{}</a>".format(doi, name)
+
+
+def get_db_nodes_dict():
+    """Given return a dictionary with all the curated materials having the material label as key, and a dict of
+    curated nodes as value."""
+
+    qb = QueryBuilder()
+    qb.append(Group, filters={'label': {'like': r'curated-cof%'}}, tag='curated_groups', project=['label'])
+    qb.append(Node, filters={'extras': {'has_key': TAG_KEY}}, with_group='curated_groups', project=['*'])
+
+    db_nodes_dict = {}
+    for q in qb.all():
+        mat_label = q[0].split("_")[1]
+        if mat_label not in db_nodes_dict:
+            db_nodes_dict[mat_label] = {}
+        n = q[1]
+        db_nodes_dict[mat_label][n.extras[TAG_KEY]] = n
+
+    return db_nodes_dict
 
 
 def get_table():
-    """Get the entries for the right table of select-figure
+    """Get the entries for the right table of select-figure."""
 
-    TODO: this section needs to be fixed if more version are present for the same COF!
-    """
+    pd.set_option('max_colwidth', 1000)
 
-    from aiida.orm.querybuilder import QueryBuilder
-    from aiida.orm import WorkFunctionNode, Node, Group
-    pandas.set_option('max_colwidth', 1000)
+    df = pd.DataFrame(columns=[  # Set the order of the columns
+        'CURATED-COFs ID', 'Article', 'Original Structure', 'Optimized Structure', 'CCS Workflow', 'Vers.'
+    ])
 
-    # import cof-frameworks as DataFrame
-    df = pandas.read_csv(this_dir + "../data/cof-frameworks.csv")
-    df_papers = pandas.read_csv(this_dir + "../data/cof-papers.csv")
+    db_nodes_dict = get_db_nodes_dict()
 
-    # get nodes (once to make it efficient): load group, get uuid if existing, make a dicts label:uuid
-    # note: this works for pe_out because if it has pe_out it has also the previous two!
-    qb = QueryBuilder()
-    qb.append(Group, project=['label'], filters={'label': {'like': 'curated-cof\_%\_v%'}}, tag='curated-cof')
-    qb.append(Node, project=['uuid'], filters={'extras.curated-cof_tag': 'orig_cif'}, with_group='curated-cof')
-    uuid_dict_orig = {v[0].split("_")[1]: v[-1] for v in qb.all()}
-    version_dict = {v[0].split("_")[1]: v[0].split("_")[-1] for v in qb.all()}
-    qb.append(Node, project=['uuid'], filters={'extras.curated-cof_tag': 'opt_cif_ddec'}, with_group='curated-cof')
-    uuid_dict_opt = {v[0].split("_")[1]: v[-1] for v in qb.all()}
-    qb.append(Node, project=['uuid'], filters={'extras.curated-cof_tag': 'pe_out'}, with_group='curated-cof')
-    uuid_dict_pe = {v[0].split("_")[1]: v[-1] for v in qb.all()}
+    for mat_id, mat_dict in db_nodes_dict.items():
+        new_row = {
+            'CURATED-COFs ID': mat_id,
+            'Article': doi_link(mat_dict),
+            'Original Structure': provenance_link(mat_dict['orig_cif'].uuid),
+            'Vers.': mat_dict['orig_cif'].extras['workflow_version'],
+        }
 
-    # search if orig_cif (ref), opt_cif and pe_out are present in the aiida database as nodes and return a new df
-    paper_link = []
-    orig_cif = []
-    opt_cif = []
-    detail = []
-    version = []
+        if 'opt_cif_ddec' in mat_dict:
+            new_row['Optimized Structure'] = provenance_link(mat_dict['opt_cif_ddec'].uuid)
+        else:
+            new_row['Optimized Structure'] = "N/A"
 
-    for index, row in df.iterrows():
-        label = row['CURATED-COFs ID']
-        paper_link += [get_paper_link(label, row['Name'], df_papers)]
+        if 'appl_pecoal' in mat_dict:
+            new_row['CCS Workflow'] = detail_link(mat_id)
+        else:
+            new_row['CCS Workflow'] = "N/A"
 
-        try:
-            orig_cif += [provenance_link(uuid_dict_orig[label])]
-            version += [version_dict[label]]
-        except KeyError:
-            orig_cif += ["N/A"]
-            version += ["N/A"]
+        df = df.append(new_row, ignore_index=True)
 
-        try:
-            opt_cif += [provenance_link(uuid_dict_opt[label])]
-        except KeyError:
-            opt_cif += ["N/A"]
-
-        try:
-            uuid_dict_pe[label]  #will fail if absent
-            detail += [detail_link(label)]
-        except KeyError:
-            detail += ["N/A"]
-
-    df['Article'] = pandas.Series(paper_link, index=df.index)
-    df['Original Structure'] = pandas.Series(orig_cif, index=df.index)
-    df['Optimized Structure'] = pandas.Series(opt_cif, index=df.index)
-    df['CCS Workflow'] = pandas.Series(detail, index=df.index)
-    df['Vers.'] = pandas.Series(version, index=df.index)
-    df.index += 1  # start indexing from 1, for better human reading
-    return df[['CURATED-COFs ID', 'Article', 'Original Structure', 'Optimized Structure', 'CCS Workflow', 'Vers.']]
+    df = df.sort_values(by=['CURATED-COFs ID'])
+    df = df.reset_index(drop=True)
+    df.index += 1
+    return df
 
 
 def fake_button(link, label):
