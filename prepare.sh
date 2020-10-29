5118bbcac82867d946f47e0c4d90c9e2c75ed926
Daniele Ongari <daniele.ongari@gmail.com>
2020-10-29 11:37:12 +0100

    update prepare.sh to get jsmol

--- prepare.sh
+++ prepare.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# Download jsmol
+# Download jsmol, and link it to be used for 'detail' and 'details' pages
 echo "### Donwloading jsmol"
+rm -rf jmol-14.29.22 # remove if previously existing
 wget https://sourceforge.net/projects/jmol/files/Jmol/Version%2014.29/Jmol%2014.29.22/Jmol-14.29.22-binary.zip/download --output-document jmol.zip
 unzip jmol.zip 
+rm jmol.zip
 cd jmol-14.29.22
-unzip jsmol.zip
-mv jsmol ../detail/static
+unzip jsmol.zip 
+rm jsmol.zip
 cd ..
-rm -r jmol-14.29.22
+rm -f ./detail/static/jsmol ./details/static/jsmol # remove if previously existing
+ln -s $(readlink -f jmol-14.29.22/jsmol) ./detail/static/jsmol  
+ln -s $(readlink -f jmol-14.29.22/jsmol) ./details/static/jsmol
